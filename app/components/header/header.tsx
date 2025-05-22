a181ee9bed696782a4a513329823f2160bfbb9d0
João Victor de Carvalho Montenegro <joritodev@gmail.com>
2025-05-22 16:42:08 -0300

    update providers

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import { dateFormatter} from "./dateFormatter";
 import "./header.scss"
+import { useState, useEffect } from "react";
 
 export function Header() {
-  const date = new Date();
+  const [currentDate, setCurrentDate] = useState(new Date());
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
   
   return (
     <header>
@@ -14,8 +23,8 @@ export function Header() {
         height={36}
         priority
       />
-      <p className="welcome">Bem vindo de volta João</p>
-      <p className="date">{dateFormatter(date)}</p>
+      <p className="welcome">Bem vindo de volta, João</p>
+      <p className="date">{dateFormatter(currentDate)}</p>
     </header>
   );
 }
